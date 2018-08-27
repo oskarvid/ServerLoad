3a4cadc1a1ed635be49eb4d0bd5f1ceb93544deb
oskarvid <oskarvidarsson@gmail.com>
2018-08-27 13:48:47 +0200

    Updated main script with a file syntax test

--- get-graphs.sh
+++ get-graphs.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
+
+set -e
+
+usage() { echo "Usage: $0 outputfile-from-the-serverload.sh-script" 1>&2; exit 1; }
+
 if [[ $# -eq 0 ]]; then
-	echo "Input file is missing." && exit 1
+	echo "Input file is missing." && usage && exit 1
 fi
 
 if [[ ! -e $1 ]]; then
-	echo "$1 does not exist" && exit 1
+	echo "$1 does not exist" && usage && exit 1
 fi
 
-docker run --rm -ti -u $(id -u $USER):$(id -g $USER) -v $(pwd):/data r-base Rscript /data/systemLoad.R /data/$1
+# This is also very hacky. It checks if the syntax of the input file is correct
+echo "Checking input file syntax"
+head $1 | grep -E "[0-9]*\.[0-9]* [0-9]*\.[0-9]* [0-9]*\.[0-9]* [0-9]*/[0-9]* [0-9]*.[0-9]{2}\:[0-9]{2}.procs_blocked [0-9]*.[0-9]*.[0-9]*G.[0-9]*G.[0-9]*G.[0-9]*.[0-9]*.[0-9]*.[0-9]*" 1>/dev/null
+echo "It checks out"
 
+echo "Running R script"
+docker run --rm -ti -u $(id -u $USER):$(id -g $USER) -v $(pwd):/data r-base Rscript /data/systemLoad.R /data/$1
+echo "All done"
